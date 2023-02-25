291872cb52a2ed38a14ddf4093380a9b10465adb
Владимир <anfalow_w@rambler.ru>
2023-02-25 17:40:38 +0400

    modifited: components/widgets/navbar, store/index.js create: data/menu moduses/menu.js

--- store/index.js
+++ store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import layout from './modules/layout'
+import menu from './modules/menu';
 Vue.use(Vuex);
 const createStore = () => {
     return new Vuex.Store({
         modules: {
-            layout
+            layout,
+            menu
         },
     });
 };
