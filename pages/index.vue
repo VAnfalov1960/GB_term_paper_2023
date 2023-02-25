2674fd46db5f7adf7f01de37a24747571f2c85de
Владимир <anfalow_w@rambler.ru>
2023-02-25 18:14:04 +0400

    modified: pages/index.vue, add: pagrs/shop/components/collection_banner.vue - добавлен на Главную страницу Коллекционный баннер с подключенными ссылками для перехода в магазины

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
     <h1>Стартовая страница</h1>
+    <CollectionBanner />
   </div>
 </template>
+
+<script>
+import CollectionBanner from "./shop/components/collection_banner";
+export default {
+  components: {
+    CollectionBanner
+  }
+};
+</script>
