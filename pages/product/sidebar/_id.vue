2fbd23cbc6abcecf3d1cfce5ec5870614dee0bcc
Владимир <anfalow_w@rambler.ru>
2023-03-08 21:08:54 +0400

    Удаление ненужного import

--- pages/product/sidebar/_id.vue
+++ pages/product/sidebar/_id.vue
@@ -16,7 +16,8 @@
                           v-for="(product,index) in getDetail.images"
                           :key="index"
                         >
-                          <img v-if="!product.src_big"
+                          <img
+                            v-if="!product.src_big"
                             :src="getImgUrl(product.src)"
                             :id="product.image_id"
                             class="img-fluid bg-img"
@@ -26,7 +27,6 @@
                         </div>
                       </div>
                     </div>
-
                   </div>
                   <div class="col-lg-6 rtl-text">
                     <div class="product-right">
@@ -44,95 +44,93 @@
     <Footer />
   </div>
 </template>
-<script src="./vue-lazytube.umd.min.js"></script>
 <script>
-  import Vue from "vue";
-  import Embed from "v-video-embed";
-  import { mapState, mapGetters } from "vuex";
-  import Footer from "../../../components/footer/footer";
-  import Breadcrumbs from "../../../components/widgets/breadcrumbs";
+import Vue from "vue";
+import Embed from "v-video-embed";
+import { mapState, mapGetters } from "vuex";
+import Footer from "../../../components/footer/footer";
+import Breadcrumbs from "../../../components/widgets/breadcrumbs";
 
-  Vue.use(Embed);
+Vue.use(Embed);
 
-  export default {
-    components: {
-      Footer,
-      Breadcrumbs
-    },
+export default {
+  components: {
+    Footer,
+    Breadcrumbs
+  },
 
-    data() {
-      return {
-        counter: 0,
-        activeColor: "",
-        selectedSize: "",
-        qty: "",
-        size: [],
-        productTYpe: "",
-        productId: "",
-        swiperOption: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-          freeMode: true
-        },
-        swiperOption1: {
-          slidesPerView: 3,
-          spaceBetween: 30,
-          freeMode: true,
-          centeredSlides: false
-        },
-      };
-    },
-    computed: {
-      // ...mapState({
-      //   currency: state => state.products.currency
-      // }),
-      // ...mapGetters({
-      //   curr: "products/changeCurrency"
-      // }),
-      getDetail: function() {
-        return this.$store.getters["products/getProductById"](
-          this.$route.params.id
-        );
+  data() {
+    return {
+      counter: 0,
+      activeColor: "",
+      selectedSize: "",
+      qty: "",
+      size: [],
+      productTYpe: "",
+      productId: "",
+      swiperOption: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        freeMode: true
       },
-      swiper() {
-        return this.$refs.mySwiper.swiper;
+      swiperOption1: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        freeMode: true,
+        centeredSlides: false
       }
+    };
+  },
+  computed: {
+    // ...mapState({
+    //   currency: state => state.products.currency
+    // }),
+    // ...mapGetters({
+    //   curr: "products/changeCurrency"
+    // }),
+    getDetail: function() {
+      return this.$store.getters["products/getProductById"](
+        this.$route.params.id
+      );
     },
-    mounted() {
-    },
-    methods: {
-      handleClick(event) {
-        this.$refs["lazyVideo"][event]();
-      },
-      priceCurrency: function() {
-        this.$store.dispatch("products/changeCurrency");
-      },
-      addToWishlist: function(product) {
-        this.$store.dispatch("products/addToWishlist", product);
-      },
-      discountedPrice(product) {
-        const price = product.price - (product.price * product.discount) / 100;
-        return price;
-      },
-      // Related Products display
-      relatedProducts() {
-        this.productTYpe = this.getDetail.type;
-        this.productId = this.getDetail.id;
-      },
-
-      increment() {
-        this.counter++;
-      },
-      decrement() {
-        if (this.counter >= 1) this.counter--;
-      },
-
-      getImgUrl(path) {
-        return require("@/assets/images/" + path);
-      },
-      slideTo(id) {
-        this.swiper.slideTo(id, 1000, false);
-      }
+    swiper() {
+      return this.$refs.mySwiper.swiper;
     }
-  };
+  },
+  mounted() {},
+  methods: {
+    handleClick(event) {
+      this.$refs["lazyVideo"][event]();
+    },
+    priceCurrency: function() {
+      this.$store.dispatch("products/changeCurrency");
+    },
+    addToWishlist: function(product) {
+      this.$store.dispatch("products/addToWishlist", product);
+    },
+    discountedPrice(product) {
+      const price = product.price - (product.price * product.discount) / 100;
+      return price;
+    },
+    // Related Products display
+    relatedProducts() {
+      this.productTYpe = this.getDetail.type;
+      this.productId = this.getDetail.id;
+    },
+
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      if (this.counter >= 1) this.counter--;
+    },
+
+    getImgUrl(path) {
+      return require("@/assets/images/" + path);
+    },
+    slideTo(id) {
+      this.swiper.slideTo(id, 1000, false);
+    }
+  }
+};
 </script>
